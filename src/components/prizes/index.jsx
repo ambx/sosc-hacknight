dcb8bedd05de100fbbfc344a291a6f6e2f391329
dhanushp <dhanushp.work@gmail.com>
2020-09-19 16:42:37 +0530

    Fixed responsiveness in prize section & centered elements in landing page

--- src/components/prizes/index.jsx
+++ src/components/prizes/index.jsx
@@ -2,26 +2,22 @@ import React from 'react';
 import styles from './styles.module.scss'
 
 function Prizes(){
-    return(
-        <div className={styles.prizes}>
-            <h1 className={styles.title}>Prizes</h1>
-            <div className={styles.content}>
-            <div className={styles.prizeCard}>
-                <ul>
-                    <li>
-                        <h1 className-={styles.amount}>₹ 20,000</h1>
-                        <p className={styles.position}>1st Prize</p>
-                    </li>
-                    <li>
-                        <h1 className-={styles.amount}>₹ 10,000</h1>
-                        <p className={styles.position}>Runner Up</p>
-                    </li>
-                </ul>
-            </div>
-
-            </div>
+    return (
+      <section className={styles.prizes}>
+        <h1 className={styles.title}>Prizes</h1>
+        <div className={styles.content}>
+          <div className={styles.prizeCard}>
+                <h1 className-={styles.amount}>₹ 20,000</h1>
+                <p className={styles.position}>1st Prize</p>
         </div>
-    )
+
+                <div className={styles.prizeCard}>
+                <h1 className-={styles.amount}>₹ 10,000</h1>
+                <p className={styles.position}>Runner Up</p>
+                </div>
+          </div>
+      </section>
+    );
 }
 
 export default Prizes
