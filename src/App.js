ff927195a5a03a29c93efbc3e5e2be4c275e0163
Hiba <hiba.hf123@gmail.com>
2020-09-19 14:12:25 +0530

    added prize cards

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import styles from './app.module.scss'
 function App() {
   return (
     <div className={styles.App}>
+          <Navbar />
           <Hero />
           <About />
           <Prizes />
--- src/components/prizes/index.jsx
+++ src/components/prizes/index.jsx
@@ -4,8 +4,20 @@ import styles from './styles.module.scss'
 function Prizes(){
     return(
         <div className={styles.prizes}>
-            <h1>Prizes</h1>
+            <h1 className={styles.title}>Prizes</h1>
             <div className={styles.content}>
+            <div className={styles.prizeCard}>
+                <ul>
+                    <li>
+                        <h1 className-={styles.amount}>₹ 20,000</h1>
+                        <p className={styles.position}>1st Prize</p>
+                    </li>
+                    <li>
+                        <h1 className-={styles.amount}>₹ 10,000</h1>
+                        <p className={styles.position}>Runner Up</p>
+                    </li>
+                </ul>
+            </div>
 
             </div>
         </div>
